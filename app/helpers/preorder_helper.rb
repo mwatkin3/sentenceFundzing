7834141ee460b2559e727d80700e44a505b8c532
mwatkin3 <mwatkin3@masonlive.gmu.edu>
2015-01-13 22:11:10 -0500

    Repositioning social media buttons

--- app/helpers/preorder_helper.rb
+++ app/helpers/preorder_helper.rb
@@ -1,6 +1,6 @@
 module PreorderHelper
   def like_button(width = 70, show_faces = false)
-    raw "<div class=\"fb-like\" data-send=\"false\" data-width=\"#{width}\" data-layout=\"box_count\" data-show-faces=\"true\"></div>"
+    raw "<div class=\"fb-like\" data-send=\"false\" data-width=\"#{width}\" data-layout=\"button_count\" data-show-faces=\"true\"></div>"
   end
   def pin_it_button
     image_url = URI.encode("#{request.scheme}://#{request.host}#{image_path(Settings.product_image_path)}")
